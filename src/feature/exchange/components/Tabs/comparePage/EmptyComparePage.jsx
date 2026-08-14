28732d33fb99546f750bc651cc20364ffd407e51
Mostafa RVslan <raslanspeed007@gmail.com>
2026-08-14 09:41:34 +0300

    Empty Page structure

---
+++ src/feature/exchange/components/Tabs/comparePage/EmptyComparePage.jsx
@@ -0,0 +1,13 @@
+import styles from "./ComparePage.module.css";
+
+export default function EmptyComparePage() {
+  return (
+    <section className={styles.page}>
+      <h2 className={styles.title}>No comparison available</h2>
+      <h3 className={styles.text}>
+        Enter an amount in Send above to see what your money is worth in other
+        currencies.
+      </h3>
+    </section>
+  );
+}
